cd06c68e745d401005ede23d65b306d5db607f3b
hakraj <adekunleddream@gmail.com>
2024-12-31 02:40:54 +0100

    ...

--- src/pages/dashboard/DashboardHome.tsx
+++ src/pages/dashboard/DashboardHome.tsx
@@ -13,7 +13,7 @@ const DashboardHome = ({ today }: { today: string }) => {
     id: "",
     name: "",
     type_format: "",
-    date_modified: "" as unknown as Date,
+    date_modified: new Date,
     status: ""
   }))
 
